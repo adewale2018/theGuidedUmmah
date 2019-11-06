80e41220129dba1e025ffa9b03343b63c531e103
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-11-06 02:05:37 +0100

    Set up the project directory and installations

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>REACT IS HERE...
+      <i class="fas fa-quran"/></h1>
+      
+    </div>
+  );
+}
+
+export default App;
